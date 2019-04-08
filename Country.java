c18d08eb4422e580ce062723e0465e80b5f58c1a
DrShpak <45294338+DrShpak@users.noreply.github.com>
2019-04-08 16:38:55 +0300

    Create Country.java

---
+++ Country.java
@@ -0,0 +1,49 @@
+package csv;
+
+import java.util.LinkedHashMap;
+
+public class Country {
+
+    private String name;
+    private LinkedHashMap<Integer, Integer> population;
+    private double changePopulation;
+
+    Country() {
+
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LinkedHashMap<Integer, Integer> getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(LinkedHashMap<Integer, Integer> population) {
+        this.population = population;
+    }
+
+    public double getChangePopulation() {
+        return changePopulation;
+    }
+
+    public void setChangePopulation(double changePopulation) {
+        this.changePopulation = changePopulation;
+    }
+
+    @Override
+    public String toString() {
+        return name +
+                "\n 2013 year: " + population.get(2013) +
+                "\n 2014 year: " + population.get(2014) +
+                "\n 2015 year: " + population.get(2015) +
+                "\n 2016 year: " + population.get(2016) +
+                "\n средний прирост (в процентах) " + changePopulation + " %" +
+                "\n";
+    }
+}
